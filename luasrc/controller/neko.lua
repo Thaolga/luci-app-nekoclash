abd973521b936d7698e05aed0f6d501acce80610
Auroraxpm <73517150+Auroraxpm@users.noreply.github.com>
2024-09-10 22:47:05 +0800

    Update version to 1.2.8.

--- luasrc/controller/neko.lua
+++ luasrc/controller/neko.lua
@@ -1,4 +1,12 @@
 module("luci.controller.neko", package.seeall)
+
 function index()
-entry({"admin","services","neko"}, template("neko"), _("NekoClash"), 13).leaf=true
+    entry({"admin", "services", "neko"}, firstchild(), _("NekoClash"), 13).leaf = false
+    entry({"admin", "services", "neko", "neko"}, template("neko"), _("Home"), 14).leaf = true
+    entry({"admin", "services", "neko", "upload"}, template("neko_upload"), _("Mihomo File Manager"), 15).leaf = true
+    entry({"admin", "services", "neko", "upload_sb"}, template("neko_upload_sb"), _("Sing-box File Manager"), 16).leaf = true
+    entry({"admin", "services", "neko", "box"}, template("neko_box"), _("Sing-box Conversion Template"), 17).leaf = true
+    entry({"admin", "services", "neko", "mon"}, template("neko_mon"), _("Sing-box Monitoring Panel"), 18).leaf = true
+    entry({"admin", "services", "neko", "yacd"}, template("neko_yacd"), _("Yacd"), 19).leaf = true
+    entry({"admin", "services", "neko", "metacubexd"}, template("neko_metacubexd"), _("MetaCubeXD"), 20).leaf = true
 end
