f6c6e3b1247c7a8f45f1e0856466f2ba887cc39c
Auroraxpm <73517150+Auroraxpm@users.noreply.github.com>
2024-09-17 16:03:13 +0800

    Update neko.lua

--- luci-app-nekoclash/luasrc/controller/neko.lua
+++ luci-app-nekoclash/luasrc/controller/neko.lua
@@ -2,8 +2,8 @@ module("luci.controller.neko", package.seeall)
 
 function index()
     entry({"admin", "services", "neko"}, firstchild(), _("NekoClash"), 1).leaf = false
-    entry({"admin", "services", "neko", "neko"}, template("neko"), _("基本设置"), 2).leaf = true
-    entry({"admin", "services", "neko", "mon"}, template("neko_mon"), _("订阅管理"), 3).leaf = true
+    entry({"admin", "services", "neko", "neko"}, template("neko"), _("Home"), 2).leaf = true
+    entry({"admin", "services", "neko", "mon"}, template("neko_mon"), _("File Manager"), 3).leaf = true
     entry({"admin", "services", "neko", "yacd"}, template("neko_yacd"), _("Yacd"), 4).leaf = true
     entry({"admin", "services", "neko", "metacubexd"}, template("neko_metacubexd"), _("MetaCubeXD"), 5).leaf = true
 end
