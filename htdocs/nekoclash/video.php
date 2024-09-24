36c24b4326ccd76dc52796855ed116dea70b8ab1
Auroraxpm <73517150+Auroraxpm@users.noreply.github.com>
2024-09-24 16:24:22 +0800

    Update video.php

--- htdocs/nekoclash/video.php
+++ htdocs/nekoclash/video.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="./assets/img/favicon.png">
     <style>
         body {
             font-family: Arial, sans-serif;
